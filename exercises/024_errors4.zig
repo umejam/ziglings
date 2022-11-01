79e6a90dd4231439629c81f921338e391a80e3bb
Takeshi Imai <imai-t@casio.co.jp>
2022-11-01 11:14:41 +0900

    021〜026完了
    エラー処理

--- exercises/024_errors4.zig
+++ exercises/024_errors4.zig
@@ -44,10 +44,7 @@ fn makeJustRight(n: u32) MyNumberError!u32 {
 
 fn fixTooBig(n: u32) MyNumberError!u32 {
     return fixTooSmall(n) catch |err| {
-        if (err == MyNumberError.TooBig) {
-            return 20;
-        }
-
+        if (err == MyNumberError.TooBig) return 20;
         return err;
     };
 }
@@ -59,7 +56,11 @@ fn fixTooSmall(n: u32) MyNumberError!u32 {
     // If we get a TooSmall error, we should return 10.
     // If we get any other error, we should return that error.
     // Otherwise, we return the u32 number.
-    return detectProblems(n) ???;
+    return detectProblems(n) catch |err| {
+        if (err == MyNumberError.TooSmall) return 10;
+        if (err == MyNumberError.TooBig) return err;
+        return n;
+    };
 }
 
 fn detectProblems(n: u32) MyNumberError!u32 {
